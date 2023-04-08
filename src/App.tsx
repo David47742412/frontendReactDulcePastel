c01895da20189c5abf6f4cd7c9e916e79de5a204
David <davidcastroastudillo20@gmail.com>
2023-04-08 12:02:30 -0500

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import {GenericView} from "./model/utility/GenericView";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-        <h1>Hello world!</h1>
-    </>
-  );
+    const [datos, setDatos] = useState<GenericView[]>([]);
+
+    useEffect(() => {
+        const ws = new WebSocket("wss://localhost:7154/ws");
+        ws.onopen = () => {
+            console.log("connected");
+            ws.send("connect");
+        };
+
+        ws.onmessage = (event: MessageEvent) => {
+            console.log("message received");
+            setDatos(JSON.parse(event.data));
+        };
+
+        ws.onclose = () => {
+            console.log("disconnected");
+        };
+    }, []);
+
+    return (
+        <>
+            {datos.map(d => (
+                <h1 key={d.Value1}>{d.Value2}</h1>
+            ))}
+        </>
+    );
 }
 
 export default App;
---
+++ src/model/utility/GenericView.ts
@@ -0,0 +1,18 @@
+
+export interface GenericView {
+    Value1: any;
+    Value2: any;
+    Value3: any;
+    Value4: any;
+    Value5: any;
+    Value6: any;
+    Value7: any;
+    Value8: any;
+    Value9: any;
+    Value10: any;
+    Value11: any;
+    Value12: any;
+    Value13: any;
+    Value14: any;
+    Value15: any;
+}
