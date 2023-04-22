e9c787fc6c1387eae71479517eff051f6a0bba3d
David <davidcastroastudillo20@gmail.com>
2023-04-21 20:15:32 -0500

    cambios

--- src/view/main/Main.tsx
+++ src/view/main/Main.tsx
@@ -4,6 +4,7 @@ import {User} from "../../model/user/dto/User";
 
 export const Main = () => {
 	const user = JSON.parse(localStorage.getItem(process.env.REACT_APP_SESSION as string) as string) as User;
+	console.log(user);
 	return (
 		<>
 			{ user == null ? window.location.href = "/login" :
